d339a4481ac227009477a00b00d17f747472628c
Jb Yoon <72485768+Jetbobs@users.noreply.github.com>
2022-03-17 18:49:29 +0900

    second commit er

--- test.js
+++ test.js
@@ -1,19 +1,9 @@
 $(function(){
-    $('.estimate_contents .wifi_box input[type="radio"]').on("change",function(){
-        let vals_wifi = $(this).val();
-        let pic_wifi = $('.estimate_contents .perpc_hidden_wifi')
-    
-        if(vals_wifi == "Y_wifi"){
-            pic_wifi.css("display","block");
-        } else {
-            pic_wifi.css("display","none");
-        }
-    })
-    $('.estimate_contents .ip_box input[type="radio"]').on("change",function(){
+    $('.estimate_contents .fixed_ip_box input[type="radio"]').on("change",function(){
         let vals_ip = $(this).val();
-        let pic_ip = $('.estimate_contents .perpc_hidden_ip')
+        let pic_ip = $('.estimate_contents .perpc_hidden_fixed_ip')
     
-        if(vals_ip == "Y_ip"){
+        if(vals_ip == "Y_office_ip"){
             pic_ip.css("display","block");
         } else {
             pic_ip.css("display","none");
